176406ebcfc34d0ee924480baafb9271298200fc
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-11-18 17:53:24 +0100

    Filter products based on budget mode in Products component

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,14 @@
 import axios from "axios"
-import { useState, useEffect, useEffectEvent } from "react"
+import { useState, useEffect, useContext } from "react"
 import { Link } from "react-router-dom"
+
+import { BudgetContext } from "../Contexts/BudgetContext"
 
 export default function Prodotti() {
     const api = 'https://fakestoreapi.com/products'
     const [products, setProducts] = useState([])
+
+    const { budgetMode } = useContext(BudgetContext)
 
     useEffect(() => {
         axios.get(api)
@@ -15,12 +19,14 @@ export default function Prodotti() {
             .catch(err => console.error('Errore', err))
     }, [])
 
+    const filteredProducts = budgetMode ? products.filter(product => product.price <= 30) : products
+
 
     return (
         <div className="container">
             <h1 className="my-4">Prodotti</h1>
             <div className="row">
-                {products.map(product => (
+                {filteredProducts.map(product => (
 
                     <div key={product.id} className="col-3 mb-4">
                         <Link to={`/prodotti/${product.id}`} className="card text-decoration-none" style={{ minHeight: '350px' }}>
