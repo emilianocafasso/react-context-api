c295cc58b979da42d31ea3cd48e6ff252671b812
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-11-18 17:25:07 +0100

    Implement BudgetContext and BudgetProvider for budget management

--- src/Contexts/BudgetContext.jsx
+++ src/Contexts/BudgetContext.jsx
@@ -1,2 +1,19 @@
 import { createContext, useState } from "react";
 
+export const BudgetContext = createContext()
+
+export default function BudgetProvider({ children }) {
+
+    const [budgetMode, setBudgetMode] = useState(false)
+}
+
+const value = {
+    budgetMode,
+    setBudgetMode
+}
+
+return (
+    <BudgetContext.Provider value={value}>
+        {children}
+    </BudgetContext.Provider>
+)
